0e7e67a1234b6fd6c38f4fc4abc872f57454ff09
Aleksander <yosestartup@gmail.com>
2020-03-20 13:17:49 +0200

    Add dataProvider & resolver

---
+++ YouGovTestowe/Assembly/DIResolver.swift
@@ -0,0 +1,21 @@
+//
+//  DIResolver.swift
+//  YouGovTestowe
+//
+//  Created by Oleksandr Bambulyak on 20/03/2020.
+//  Copyright © 2020 Oleksandr Bambulyak. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - DIResolver
+class DIResolver {
+
+    let dataProvider: DataProvider
+
+    // MARK: - Init
+    init(dataProvider: DataProvider) {
+        self.dataProvider = dataProvider
+    }
+
+}
---
+++ YouGovTestowe/DataLayer/DataProvider.swift
@@ -0,0 +1,13 @@
+//
+//  DataProvider.swift
+//  YouGovTestowe
+//
+//  Created by Oleksandr Bambulyak on 20/03/2020.
+//  Copyright © 2020 Oleksandr Bambulyak. All rights reserved.
+//
+
+import Foundation
+
+class DataProvider {
+    
+}
